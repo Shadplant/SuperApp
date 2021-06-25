6d41d492bafcb2516d5f39fed4dbfb7523df2d17
Daniel <Dan.Dyushko.hgjh95@gmail.com>
2021-06-25 13:55:00 +0300

    Methods add

---
+++ MyDll/DapperFacade.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MyDll
+{
+    public class DapperFacade
+    {
+        private const string connetionString = "Data Source=server1;Initial Catalog=SupperAppdb;User Id=student;";
+
+        public DapperFacade() { }
+
+        public bool CheckLogin(string login)
+        {
+            using (IDbConnection conn = new SqlConnection(connetionString))
+            {
+                if (conn.Query<object>($"EXEC Check_Login {login}").AsList().Count > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool Authorize(string login, string pass)
+        {
+            using (IDbConnection conn = new SqlConnection(connetionString))
+            {
+                if (conn.Query<object>($"EXEC Authorize {login}, {pass}").AsList().Count > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
